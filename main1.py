1a7a539abff8849e46375bc73033d8c742f997a2
Andreea-Adriana Perdun <andreea.perdun@stud.ubbcluj.ro>
2022-02-16 20:17:05 +0200

    Lab 3

--- main1.py
+++ main1.py
@@ -1,136 +1,119 @@
-#cerintele 4, 10 si 13
-def citire():
-    '''
-    Citim elementele listei
-    :return: elementele listei
-    '''
-    l = []
-    n = int(input("Dati numarul de elemente: "))
-    for i in range(n):
-        l.append(int(input(f"l[{i}]= ")))
-    return l
-
-def numere_prime(l):
-    '''
-    Functie de arata daca toate elementele unui subliste sunt prime.
-    :param l: sublista cu numere intregi
-    :return: True daca toate elemntele sunt prime, False in caz contrar
-    '''
-    for a in l:
-        while a != 0:
-            c = a % 10
-            if c == 0:
-                return False
-            for b in range(2,c // 2 + 1):
-                if c % b == 0:
-                    return False
-            a = a//10
-    return True
-
-def numere_pare(l):
-    """
-    Functie ce returneaza True daca toate elementele unei subliste sunt pare.
-    :param l: lista de numere
-    :return: True daca toate sunt pare, False in caz contrar
-    """
-    for x in l:
-        if x % 2 != 0:
-            return False
-    return True
+import math
 
 
-def get_longest_all_even(l):
-    """
-    Determina cea mai lunga secventa care are toate numerele pare
-    :param l: lista cu numerele
-    :return: secventa cea mai lunga de numere
-    """
-    subsecmax=[]
-    for i in range(len(l)):
-        for j in range(i, len(l)):
-            if numere_pare(l[i:j+1]) and len(subsecmax) < len(l[i:j+1]):
-                subsecmax= l[i:j+1]
-    return subsecmax
-
-
-def test_get_longest_all_even(l):
-    assert get_longest_all_even([2,4,6,8,9,]) == [2,4,6,8]
-    assert get_longest_all_even([1,3,5]) == []
-    assert get_longest_all_even([1,9,10,3]) == [10]
-
-
-def ordonare(l):
-    '''
-    Functia returneaza True daca toate elementele unei subsecvente sunt ordonate crescator.
-    :param l: Lista de numere
-    :return: True daca sunt ordonate crescator, False in caz contrar
-    '''
-    for x in l:
-        if x < x + 1:
-            return False
-    return True
-
-def get_longest_sorted_asc(l):
-    '''
-    Gasim cea mai lunga subsecventa de elemente din lista, ordonate crescator
-    :param lst: Lista elementelor
-    :return: Subsecventa cea mai lunga de numere
-    '''
-    subsecmax = []
-    for i in range(len(l)):
-        for j in range(i, len(l)):
-            if ordonare(l[i:j + 1]) and len(subsecmax) < len(l[i:j + 1]):
-                subsecmax = l[i:j + 1]
-    return subsecmax
-
-
-def test_get_longest_sorted_asc(l):
-    assert get_longest_sorted_asc([10,5]) == []
-    assert get_longest_sorted_asc([1,9,10,3]) == [1,9,10]
-
-
-def get_longest_prime_digits(l):
-    '''
-    Determina cea mai lunga secventa ce are doar numere prime.
-    :param l: lista cu numere
-    :return: secventa cea mai lunga
-    '''
-    submax= []
-    for i in range(len(l)):
-        for j in range(i, len(l)):
-            if numere_prime(l[i:j+1]) and len(submax) < len(l[i:j+1]):
-                submax= l[i:j+1]
-    return submax
-
-
-def test_get_longest_prime_digits(l):
-    assert get_longest_prime_digits([2,13,5,8,9]) == [2,13,5]
-    assert get_longest_prime_digits([6,8,9]) == []
-    assert get_longest_prime_digits([6,2,3,8,9,10,11,13,17]) == [11,13,17]
-
-def main():
-    l=[]
-    test_get_longest_all_even(l)
-    test_get_longest_prime_digits(l)
-    '''test_get_longest_sorted_asc(l)'''
-    while True:
-        print("1. Citire date.")
-        print("2. Determina cea mai lungă subsecvență cu proprietatea ca numerele sa fie ordonate crescator.")
-        print("3. Determina cea mai lungă subsecvență cu proprietatea ca numerele sa fie formate din cifre prime.")
-        print("4. Determina cea mai lunga subsecventa cu proprietatea ca numerele sa fie pare.")
-        print("5. Iesire")
-        op=input("Dati optiunea: ")
-        if op == "5":
-            break
-        elif op == "1":
-            citire()
-        elif op == "2":
-            print(get_longest_sorted_asc(l))
-        elif op == "3":
-            print(get_longest_prime_digits(l))
-        elif op == "4":
-            print(get_longest_all_even(l))
+def get_longest_all_perfect_squares(lst: list):
+    longest=0
+    longest_poz=0
+    lungime_actuala=0
+    actual_poz=-1
+    poz=-1
+    for i in lst:
+        poz=poz+1
+        if math.sqrt(i)==math.isqrt(i) and i!=0:
+            lungime_actuala=lungime_actuala+1
+            if lungime_actuala==1:
+                actual_poz=poz
         else:
-            print("!!!Optiunea nu este buna!!!")
+            lungime_actuala=0
+            actual_poz=0
+        if lungime_actuala>longest:
+            longest=lungime_actuala
+            longest_poz=actual_poz
+    lst2=[]
+    if actual_poz != -1:
+        for i in range(longest_poz, longest_poz + longest):
+            lst2.append(lst[i])
+    return lst2
 
+
+def get_longest_average_below(lst, average):
+    ceamailunga=0
+    poz=-1
+    for i in range (0,len(lst)):
+        for j in range(i+1,len(lst)):
+            suma=0
+            for k in range (i,j+1):
+                if k!=len(lst):
+                    suma=suma+lst[k]
+            if suma/(j-i+1)<average and ceamailunga<j-i+1:
+                ceamailunga=j-i
+                poz=i
+    lst2=[]
+    if poz!=-1:
+        for i in range(poz,ceamailunga+poz+1):
+            lst2.append(lst[i])
+    return lst2
+
+
+def get_longest_div_k(lst,k):
+    longest = 0
+    longest_poz = 0
+    lungime_actuala = 0
+    actual_poz = -1
+    poz = -1
+    for i in lst:
+        poz = poz + 1
+        if i%k==0:
+            lungime_actuala = lungime_actuala + 1
+            if lungime_actuala == 1:
+                actual_poz = poz
+        else:
+            lungime_actuala = 0
+            actual_poz = 0
+        if lungime_actuala > longest:
+            longest = lungime_actuala
+            longest_poz = actual_poz
+    lst2 = []
+    if actual_poz != -1:
+        for i in range(longest_poz, longest_poz + longest):
+            lst2.append(lst[i])
+    return lst2
+
+def test_get_longest_all_perfect_squares():
+    assert get_longest_all_perfect_squares([])==[]
+    assert get_longest_all_perfect_squares([25])==[25]
+    assert get_longest_all_perfect_squares([0])==[]
+    assert get_longest_all_perfect_squares([10, 9, 25])==[9, 25]
+
+def test_get_longest_div_k():
+    assert get_longest_div_k([5,10,4,25],5)==[5,10]
+    assert get_longest_div_k([0,1,2,3,4],2)==[0]
+    assert get_longest_div_k([1,2,3,4,5],1)==[1,2,3,4,5]
+
+def test_get_longest_average_below():
+    assert get_longest_average_below([1,2,3],1.6)==[1,2]
+    assert get_longest_average_below([9, 10, 1990],13)==[9,10]
+    assert get_longest_average_below([1,2,3],1)==[]
+def main():
+    while True:
+        print("1.Determina cea mai lunga secventa de patrate perfecte dintr-o lista data.")
+        print("2.Determina cea mai lunga secventa cu proprietatea ca media numerelor din subsecventa este mai mica decat o valoare data .")
+        print("3.Determina cea mai lunga secventa cu proprietatea ca toate numerele din subsecventa sunt divizibile cu un numar dat.")
+        print("4.Iesire din program.")
+        optiune = input("Alege optiunea:")
+        if optiune == '1':
+            test_get_longest_all_perfect_squares()
+            lst = []
+            n = int(input("Numarul de numere:"))
+            for i in range(0, n):
+                lst.append(int(input("Dati " + str(n) + " numere:")))
+            print(get_longest_all_perfect_squares(lst))
+        elif optiune == '2':
+            test_get_longest_average_below()
+            lst = []
+            n = int(input("Numarul de numere:"))
+            for i in range(0, n):
+                lst.append(int(input("Dati " + str(n) + " numere:")))
+            average=float(input("Valoarea data pentru medie:"))
+            print( get_longest_average_below(lst,average))
+        elif optiune == '3':
+            test_get_longest_div_k()
+            lst=[]
+            n = int(input("Numarul de numere:"))
+            for i in range(0, n):
+                lst.append(int(input("Dati " + str(n) + " numere:")))
+            k = int(input("Numarul cu care trebuie sa fie divizibile elementele secventei:"))
+            print(get_longest_div_k(lst,k))
+        elif optiune == '4':
+            break
 main()
